7a04fd775266b50d36f0013d991ee561410f7825
Robert <110127@st-maartenscollege.nl>
2020-06-22 13:26:36 +0000

    added Enigma class and function to make rotors turn

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 translatedText = []
 rotors = []
+
+class Enigma:
+  def __init__(self, rotors):
+    self.rotors = rotors
+    
+# function that makes the rotors turn
+def rotorTurn(turns, rotor):
+  rotor = (rotor[-turns:] + rotor)[0: 26]
+  return rotor
